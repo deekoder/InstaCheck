c059059836907ebf75dde68c68e8c877678b8eab
Ashok Kumar <ashokGk215@gmail.com>
2014-11-07 21:18:38 -0800

    issues #21,22,23 Show TipAmount with Tip %; strings in xml; seek bar labels centered

--- src/com/moquapps/android/instacheck/TipActivity.java
+++ src/com/moquapps/android/instacheck/TipActivity.java
@@ -17,8 +17,13 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
 public class TipActivity extends Activity {
+	public final int TIP_PERCENT_MIN = 0;
+	public final int TIP_PERCENT_MAX = 30;
+	public final int TIP_PERCENT_DEFAULT = 15;
+	
 	private SeekBar seekBar; 
-	private TextView textViewProgress; 	
+	private TextView textViewTipPercent,
+	                 textViewTipAmount; 	
 	Handler seekBarHandlerToChangeProgressColor;
 	HashMap<String, Double> pTotalMap;
 	
@@ -33,22 +38,34 @@ public class TipActivity extends Activity {
 	public int mTipSplitSelection = TIP_PROPORTIONAL_SPLIT_SELECTED;//default
 	public int mTaxSplitSelection = TAX_PROPORTIONAL_SPLIT_SELECTED;//default
 	
+	double mSubTotalFromParseBill_temp = 32.11;//Get value from ParseBill1.java when 
+	                                           //it works - ak - Oct24,14 
+	Double mTipAmount;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);		
 		setContentView(R.layout.layout_tip_activity);
-		
+	    textViewTipPercent = (TextView)findViewById(R.id.textViewSeekBarProgressValue);
+	    textViewTipAmount = (TextView)findViewById(R.id.textViewTipAmount); 
 		Intent intent = getIntent();    
 		//HashMap<String, Double> pTotalMap = (HashMap<String, Double>) intent.getSerializableExtra("hashMap");
 		pTotalMap = (HashMap<String, Double>) intent.getSerializableExtra("hashMap");
 		
 		display(pTotalMap);
 		
-	      seekBar = (SeekBar)findViewById(R.id.seekBarTipPercentage);
-		   seekBar.setMax(35);
-		   seekBar.setProgress(15);
-		    
-		   seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+	    seekBar = (SeekBar)findViewById(R.id.seekBarTipPercentage);
+		seekBar.setMax(TIP_PERCENT_MAX);
+		seekBar.setProgress(TIP_PERCENT_DEFAULT);
+		//Display initial Tip Percentage
+		textViewTipPercent.setText(" " + TIP_PERCENT_DEFAULT + "%");
+		//display initial $ default Tip Amount for default 15% Tip; round to 2 decimals
+		mTipAmount = (double)Math.round(mSubTotalFromParseBill_temp *
+				                                       TIP_PERCENT_DEFAULT);
+		mTipAmount /= 100;
+		textViewTipAmount.setText(" $" + mTipAmount.toString()+ ")");
+		
+		seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 	         
 			   
 		 	 @Override 
@@ -63,17 +80,23 @@ public class TipActivity extends Activity {
 			 }
 			 
 			 @Override 
-			 public void onProgressChanged(SeekBar seekBar, int tipPercentParameter,
+			 public void onProgressChanged(SeekBar seekBar, int progressTipPercent,
 					                                boolean fromUser) {
 			 
-				seekBar.setProgress(tipPercentParameter);
-				textViewProgress.setText("Tip: " + tipPercentParameter + "%");
-				mTipPercentSelected = tipPercentParameter;
+				seekBar.setProgress(progressTipPercent);
+				textViewTipPercent.setText(" " + progressTipPercent + "%");
+				mTipPercentSelected = progressTipPercent;
+				//disp Tip Amount; round to 2 decimals
+				mTipAmount = (double)Math.round(mSubTotalFromParseBill_temp *
+						                                     progressTipPercent);
+				mTipAmount /= 100;
+				String mTipAmountStr = String.format(" $" + "%.2f", mTipAmount);					
+				textViewTipAmount.setText(mTipAmountStr + ")");								
 			 }		 
 		 });
 	    
 		   
-	     textViewProgress = (TextView)findViewById(R.id.textViewSeekBarProgressValue);
+	
 	    	   
 	} 
 	
